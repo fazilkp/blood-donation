c8803da12440a041031a9106f68d79a15f6d7607
fazilkp <fazil.amigo@gmail.com>
2017-03-18 13:52:06 +0530

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,28 @@ var config = {
    messagingSenderId: "354169837690"
  };
  firebase.initializeApp(config);
+
+
+
+
+  var $form = $('.js-form');
+  var $name = $('#firstName');
+  var $email = $('#email');
+  var $age = $('#age');
+  var $bgrp = $('#bgrp');
+  var $gender = $('#gender');
+  var $mNo= $('#mNo');
+  var $pincode= $('#pincode');
+
+  $form.on('submit', function (event){
+    event.preventDefault();
+
+    var nameValue = $name.val();
+    var emailValue = $email.val();
+    var ageValue = $age.val();
+    var bgrpValue = $bgrp.val();
+    var genderValue =$gender.val();
+    var mNoValue = $mNo.val();
+    var pincodeValue = $pincode.val();
+    console.log(nameValue, emailValue, ageValue, bgrpValue, genderValue, mNoValue, pincodeValue);
+  });
