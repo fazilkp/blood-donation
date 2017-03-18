e00e7d4de3c44bfd0e7bc2be69f1f886115351e5
fazilkp <fazil.amigo@gmail.com>
2017-03-18 14:30:35 +0530

    First commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,25 +9,46 @@ var config = {
 
 
 
-
+  var i=0;
   var $form = $('.js-form');
   var $name = $('#firstName');
   var $email = $('#email');
   var $age = $('#age');
   var $bgrp = $('#bgrp');
-  var $gender = $('#gender');
+  var $gender = "";
   var $mNo= $('#mNo');
   var $pincode= $('#pincode');
 
   $form.on('submit', function (event){
     event.preventDefault();
-
+    i++;
     var nameValue = $name.val();
     var emailValue = $email.val();
     var ageValue = $age.val();
     var bgrpValue = $bgrp.val();
-    var genderValue =$gender.val();
     var mNoValue = $mNo.val();
     var pincodeValue = $pincode.val();
+    if(document.getElementById('maleRadio').checked) {
+        var genderValue = "male";
+    }else if(document.getElementById('femaleRadio').checked) {
+        var genderValue = "female";
+    }
+
+    writeUserData(i, nameValue, emailValue, ageValue, bgrpValue, genderValue, mNoValue, pincodeValue);
     console.log(nameValue, emailValue, ageValue, bgrpValue, genderValue, mNoValue, pincodeValue);
   });
+
+
+
+  function writeUserData(userId, nameValue, emailValue, ageValue, bgrpValue, genderValue, mNoValue, pincodeValue) {
+    console.log("hai");
+  firebase.database().ref('users/' + userId).set({
+    name: nameValue,
+    email: emailValue,
+    dob: ageValue,
+    blood_grp: bgrpValue,
+    gender: genderValue,
+    phone_no: mNoValue,
+    pincode: pincodeValue
+  });
+}
