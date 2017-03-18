7ec5fab58adb382fd2783cdeed92900ae5c80bbc
fazilkp <fazil.amigo@gmail.com>
2017-03-18 16:12:21 +0530

    script added map and drag functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// firebase
+
 var config = {
    apiKey: "AIzaSyC_7nkoKQjOBQO7a7oXAeYf7wT5KkXLNyQ",
    authDomain: "blood-donate-5bb33.firebaseapp.com",
@@ -21,7 +23,6 @@ var config = {
 
   $form.on('submit', function (event){
     event.preventDefault();
-    i++;
     var nameValue = $name.val();
     var emailValue = $email.val();
     var ageValue = $age.val();
@@ -35,13 +36,13 @@ var config = {
     }
 
     writeUserData(i, nameValue, emailValue, ageValue, bgrpValue, genderValue, mNoValue, pincodeValue);
-    console.log(nameValue, emailValue, ageValue, bgrpValue, genderValue, mNoValue, pincodeValue);
+    // console.log(nameValue, emailValue, ageValue, bgrpValue, genderValue, mNoValue, pincodeValue);
   });
 
 
 
   function writeUserData(userId, nameValue, emailValue, ageValue, bgrpValue, genderValue, mNoValue, pincodeValue) {
-    console.log("hai");
+    // console.log("hai");
   firebase.database().ref('users/' + userId).set({
     name: nameValue,
     email: emailValue,
@@ -51,4 +52,63 @@ var config = {
     phone_no: mNoValue,
     pincode: pincodeValue
   });
+  i++;
+
+    //create reference
+      var dbRefObject = firebase.database().ref();
+    // sync object changes
+      dbRefObject.on('value', snap=>{
+        var fullData = JSON.stringify(snap.val());
+        console.log(fullData.users);
+
+      });
+    // sync list changes
+    // dbRefList.on('child_added', snap => console.log(snap.val()));
+
+  }
+
+
+// map
+
+
+function initMap() {
+       var uluru = {lat: 11.0445974, lng: 75.9097158};
+       var map = new google.maps.Map(document.getElementById('map'), {
+         zoom: 15,
+         center: uluru
+       });
+       var marker = new google.maps.Marker({
+         position: uluru,
+         map: map,
+         draggable:true
+       });
+
+
+       google.maps.event.addListener(marker, 'dragend', function (event) {
+           var lat = event.latLng.lat();
+           var lng = event.latLng.lng();
+           var latlng = ''+lat+','+lng+'';
+           var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+latlng+'&key=AIzaSyDyAn0b7enEaVz1zVojwb2-b9nB-AFo8GU';
+           console.log(url);
+           getPin(url);
+
+       });
+
+
+
+     }
+
+
+function getPin(url){
+
+
+  $.getJSON( url, function( data ) {
+
+    var pin = data.results[0].address_components[data.results[0].address_components.length - 1].long_name;
+    // console.log(data.results[0].address_components[data.results[0].address_components.length - 1].long_name);
+    $("#pincode_search").val(pin);
+
+});
+
+
 }
